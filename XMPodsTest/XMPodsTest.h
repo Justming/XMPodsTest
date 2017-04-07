22220288db88744e4a8e167b2bb95f3e13f79642
hxm <hxm@cto100.com>
2017-04-07 09:58:18 +0800

    init

---
+++ XMPodsTest/XMPodsTest.h
@@ -0,0 +1,13 @@
+//
+//  XMPodsTest.h
+//  XMPodTest
+//
+//  Created by hxm on 2017/4/7.
+//  Copyright © 2017年 hxm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XMPodsTest : NSObject
+
+@end
---
+++ XMPodsTest/XMPodsTest.m
@@ -0,0 +1,13 @@
+//
+//  XMPodsTest.m
+//  XMPodTest
+//
+//  Created by hxm on 2017/4/7.
+//  Copyright © 2017年 hxm. All rights reserved.
+//
+
+#import "XMPodsTest.h"
+
+@implementation XMPodsTest
+
+@end
